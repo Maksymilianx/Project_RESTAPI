21224e0186453f195f58bff17bf7dc212c5ef71d
Maksymilianx <79858139+Maksymilianx@users.noreply.github.com>
2021-06-16 23:13:35 +0200

    add unit tests

--- showtimes/tests/utils.py
+++ showtimes/tests/utils.py
@@ -5,4 +5,34 @@ from faker import Faker
 
 from moviebase.settings import TIME_ZONE
 from movielist.models import Movie
+from showtimes.models import Screening, Cinema
 
+faker = Faker("pl_PL")
+TZ = pytz.timezone(TIME_ZONE)
+
+
+def random_movies():
+    # Return 3 random movies from db
+    movies = list(Movie.objects.all())
+    return sample(movies, 3)
+
+
+def add_screenings(cinema):
+    # Add 3 screenings for given cinema
+    movies = random_movies()
+    for movie in movies:
+        Screening.objects.create(cinema=cinema, movie=movie, date=faker.date_time(tzinfo=TZ))
+
+
+def fake_cinema_data():
+    # Generate fake data for cinema
+    return {
+        "name": faker.name(),
+        "city": faker.city(),
+    }
+
+
+def create_fake_cinema():
+    # Create fake cinema with some screenings
+    cinema = Cinema.objects.create(**fake_cinema_data())
+    add_screenings(cinema)
